3c1f1e86877aa94898af052029b578ff7099d00d
Jonathan Bowers <jonotron@gmail.com>
2018-08-10 15:52:46 -0700

    add lunr, basic stateful search

--- react-lunr.js
+++ react-lunr.js
@@ -1,23 +1,46 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+
+import lunr from 'lunr'
+import moonwalkers from './moonwalkers'
 
 class ReactLunr extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log({ props, moonwalkers })
+    const index = lunr(function() {
+      this.field('name')
+      this.field('body')
+      this.ref('id')
+      moonwalkers.forEach(walker => this.add(walker))
+    })
+    this.state = { filter: '', index, results: [] }
+  }
+
+  handleChange = event => {
+    const filter = event.target.value
+    const results = this.state.index.search(filter).map(({ ref, ...rest }) => ({
+      ref,
+      item: moonwalkers.find(m => m.id === ref),
+      ...rest
+    }))
+    this.setState({ results, filter })
+  }
+
   render() {
+    const { results } = this.state
+    console.log({ results })
     return (
       <div>
-        {this.props.documents.map((doc, i) => (
+        <input onChange={this.handleChange} value={this.state.filter} />
+        {results.map((result, i) => (
           <div key={i}>
-            <h2>{doc.name}</h2>
-            <p>{doc.body}</p>
+            <h1>{result.item.name}</h1>
+            <p>{result.item.body}</p>
           </div>
         ))}
       </div>
     )
   }
-}
-
-ReactLunr.propTypes = {
-  documents: PropTypes.array
 }
 
 export default ReactLunr
--- react-lunr.story.js
+++ react-lunr.story.js
@@ -2,8 +2,5 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import ReactLunr from './react-lunr'
-import moonwalkers from './moonwalkers'
 
-storiesOf('ReactLunr', module).add('default', () => (
-  <ReactLunr documents={moonwalkers} />
-))
+storiesOf('ReactLunr', module).add('default', () => <ReactLunr />)
