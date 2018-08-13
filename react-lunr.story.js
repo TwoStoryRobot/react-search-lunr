a89d84b7cc75a4409dcc458548c6d3bf74e1bbd4
Jonathan Bowers <jonotron@gmail.com>
2018-08-13 13:24:06 -0700

    remove unnecessary constants

--- react-lunr.story.js
+++ react-lunr.story.js
@@ -1,53 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { storiesOf } from '@storybook/react'
+import { Store, StateDecorator } from '@sambego/storybook-state'
 
 import ReactLunr from './react-lunr'
 import moonwalkers from './moonwalkers'
 
-class Wrapper extends React.Component {
-  state = { filter: '' }
-
-  handleChange = event => {
-    const filter = event.target.value
-    this.setState({ filter })
-  }
-
-  render() {
-    return (
-      <div>
-        search:
-        <input onChange={this.handleChange} value={this.state.filter} />
-        {this.props.children(this.state.filter)}
-      </div>
-    )
-  }
-}
-
-Wrapper.propTypes = {
-  children: PropTypes.func
-}
-
-storiesOf('ReactLunr', module).add('default', () => {
-  const fields = ['name', 'body']
-  const id = 'id'
-  return (
-    <Wrapper>
-      {filter => (
-        <ReactLunr
-          id={id}
-          fields={fields}
-          documents={moonwalkers}
-          filter={filter}>
-          {(result, i) => (
-            <div key={i}>
-              <p>
-                <strong>{result.item.name}</strong> - {result.item.body}
-              </p>
-            </div>
-          )}
-        </ReactLunr>
-      )}
-    </Wrapper>
-  )
+const store = new Store({
+  filter: ''
 })
+
+function Input({ filter, ...rest }) {
+  return <input value={filter} {...rest} />
+}
+
+Input.propTypes = {
+  filter: PropTypes.string
+}
+
+storiesOf('ReactLunr', module)
+  .addDecorator(StateDecorator(store))
+  .add('default', () => {
+    const handleChange = e => {
+      store.set({ filter: e.target.value })
+    }
+    return [
+      <Input key="input" onChange={handleChange} />,
+      <ReactLunr
+        key="lunr"
+        id="id"
+        fields={['name', 'body']}
+        documents={moonwalkers}>
+        {(result, i) => (
+          <div key={i}>
+            <p>
+              <strong>{result.item.name}</strong> - {result.item.body}
+            </p>
+          </div>
+        )}
+      </ReactLunr>
+    ]
+  })
