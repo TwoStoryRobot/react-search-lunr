a73e3507163c856f05c888c49cac427a67889c29
Jonathan Bowers <jonotron@gmail.com>
2018-10-03 15:51:15 -0700

    test initial filter renders correctly

--- react-lunr.test.js
+++ react-lunr.test.js
@@ -1,3 +1,35 @@
-test('initial test', () => {
-  expect(true).toBe(true)
+import React from 'react'
+import { render, cleanup } from 'react-testing-library'
+import 'jest-dom/extend-expect'
+
+import ReactLunr from './react-lunr'
+
+afterEach(cleanup)
+
+test('should call children render prop with results on initial filter', async () => {
+  const documents = [
+    { id: 'a', name: 'test a', body: 'test a was found' },
+    { id: 'b', name: 'test b', body: 'test b was unknown' },
+    { id: 'c', name: 'ignore c', body: 'ignore this result' }
+  ]
+  const { queryByText } = render(
+    <ReactLunr
+      id="id"
+      documents={documents}
+      fields={['name', 'body']}
+      filter="test">
+      {results =>
+        results.map(result => (
+          <div key={result.ref}>
+            <h1>{result.item.name}</h1>
+            <p>{result.item.body}</p>
+          </div>
+        ))
+      }
+    </ReactLunr>
+  )
+
+  expect(queryByText('test a')).toBeInTheDocument()
+  expect(queryByText('test b')).toBeInTheDocument()
+  expect(queryByText('ignore c')).not.toBeInTheDocument()
 })
