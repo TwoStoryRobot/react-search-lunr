1ff56afd133fb760d6c871a86fed9aee0d4a403d
Jonathan Bowers <jonotron@gmail.com>
2018-10-04 14:55:22 -0700

    test only indexes specified fields

--- react-lunr.test.js
+++ react-lunr.test.js
@@ -6,12 +6,13 @@ import ReactLunr from './react-lunr'
 
 afterEach(cleanup)
 
+const documents = [
+  { id: 'a', name: 'test a', body: 'test a was found' },
+  { id: 'b', name: 'test b', body: 'test b was unknown' },
+  { id: 'c', name: 'ignore c', body: 'ignore this result' }
+]
+
 test('should render only results matching initial filter', async () => {
-  const documents = [
-    { id: 'a', name: 'test a', body: 'test a was found' },
-    { id: 'b', name: 'test b', body: 'test b was unknown' },
-    { id: 'c', name: 'ignore c', body: 'ignore this result' }
-  ]
   const { container, queryByText } = render(
     <ReactLunr
       id="id"
@@ -34,4 +35,22 @@ test('should render only results matching initial filter', async () => {
   expect(queryByText('ignore c')).not.toBeInTheDocument()
 
   expect(container.firstChild).toMatchSnapshot()
+})
+
+test('should only index specified fields', async () => {
+  const { queryByText } = render(
+    <ReactLunr id="id" documents={documents} fields={['name']} filter="result">
+      {results =>
+        results.map(result => (
+          <div key={result.ref}>
+            <h1>{result.item.name}</h1>
+            <p>{result.item.body}</p>
+          </div>
+        ))
+      }
+    </ReactLunr>
+  )
+
+  expect(queryByText('test a')).not.toBeInTheDocument()
+  expect(queryByText('ignore c')).not.toBeInTheDocument()
 })
